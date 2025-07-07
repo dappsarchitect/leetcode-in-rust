9f9e469ce7afd74ab387d3e4c80057b7e6deeba8
Raymond Yeung <raymond.yeung@email.com>
2025-07-07 16:02:56 +0800

    Added template file as placeholder of codes

---
+++ src/Template_for_LeetCode.rs
@@ -0,0 +1,16 @@
+"""
+Test Date: ? July, 2025 (reload)
+Runtime: ? ms (Beats ?%)
+Memory : ? MB (Beats ?%)
+"""
+
+// Code...
+
+"""
+Task Description:
+
+
+
+Credits: LeetCode (https://leetcode.com)
+Copyright ©️ 2025 LeetCode All rights reserved
+"""
